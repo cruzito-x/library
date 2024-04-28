e9751923d0113262b1b385ddde173825574f513b
David Cruz <dcruzer92@gmail.com>
2024-04-28 16:42:53 -0600

    Stable backend version

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
@@ -11,6 +12,54 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
+// Middleware para parsear el body de las solicitudes como JSON
+app.use(bodyParser.json());
+
+// Aceptar CORS de diferentes endpoints fuera del servidor
+app.use(cors({ origin: "*" }));
+
+// Configuración de la conexión a la base de datos MySQL
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "librarydb",
+  port: 3307,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Endpoint para manejar el inicio de sesión
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const query = `select * from usuarios where nombreUsuario = ? and password = ?`;
+  connection.query(query, [username, password], (err, results) => {
+    if (err) {
+      console.error("Error al ejecutar la consulta:", err);
+      res.status(500).json({ message: "Error interno del servidor" });
+      return;
+    }
+
+    if (results.length > 0) {
+      res.status(200).json({ message: "Inicio de sesión exitoso" }); // Usuario logueado exitosamente
+    } else {
+      res.status(401).json({ message: "Nombre de usuario o contraseña incorrectos" }); // Credenciales incorrectas, enviar una respuesta de error
+    }
+  });
+});
+
+// Conectar a la base de datos
+connection.connect((error) => {
+  if (error) {
+    console.error("Error de conexión a la base de datos:", error);
+    return;
+  }
+  console.log("Conexión a la base de datos MySQL exitosa");
+});
+
 // Crea un stream de escritura para el archivo de registro
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
@@ -20,72 +69,6 @@ app.use(morgan('combined', { stream: accessLogStream }));
 // Redirige stdout y stderr al mismo stream
 process.stdout.write = accessLogStream.write.bind(accessLogStream);
 process.stderr.write = accessLogStream.write.bind(accessLogStream);
-
-// Configuración de Passport.js para la autenticación local
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // Consulta a la base de datos para verificar las credenciales del usuario
-    const query = `select * from usuarios where nombreUsuario = ? and password = ?`;
-    connection.query(query, [username, password], (err, results) => {
-      if (err) {
-        return done(err);
-      }
-      if (results.length === 0) {
-        return done(null, false, { message: "Nombre de usuario o contraseña incorrectos" });
-      }
-      return done(null, results[0]);
-    });
-  }
-));
-
-// Middleware para validar la sesión del usuario
-const validarSesion = (req, res, next) => {
-  if (req.isAuthenticated()) { // Verificar si el usuario está autenticado
-    return next(); // Si está autenticado, permitir el acceso a la siguiente ruta
-  } else {
-    return res.status(401).json({ message: "Debe iniciar sesión para acceder a esta ruta" }); // Si no está autenticado, devolver un error de acceso no autorizado
-  }
-};
-
-// Middleware para parsear el body de las solicitudes como JSON
-app.use(bodyParser.json());
-
-// Aceptar accesos de origen diferente al local
-app.use(cors({ origin: "*" }));
-
-// Inicializar Passport.js
-app.use(passport.initialize());
-
-// Conectar a la base de datos
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "librarydb",
-  port: 3307,
-});
-
-connection.connect((error) => {
-  if (error) {
-    console.error("Error de conexión a la base de datos:", error);
-    return;
-  }
-  console.log("Conexión a la base de datos MySQL exitosa");
-});
-
-// Endpoint para manejar el inicio de sesión
-app.post("/login", passport.authenticate('local'), (req, res) => {
-  res.status(200).json({ message: "Inicio de sesión exitoso" });
-});
-
-// Middleware para validar la sesión del usuario en todas las rutas excepto en /login
-app.use((req, res, next) => {
-  if (req.path !== "/login") {
-    validarSesion(req, res, next);
-  } else {
-    next();
-  }
-});
 
 // Levantar el servidor
 app.listen(port, () => {
