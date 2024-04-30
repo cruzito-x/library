51f226716ad692b57881095de2b515152a88ba76
David Cruz <dcruzer92@gmail.com>
2024-04-30 12:36:42 -0600

    Updated: all backend

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,14 +1,45 @@
 const db = require("../config/db");
 
 exports.getAllBooks = (req, res) => {
-  db.query("SELECT * FROM Libros", (err, results) => {
+  db.query("select * from libros", (err, results) => {
     if (err) {
       console.error("Error al obtener los libros:", err);
       res.status(500).json({ message: "Error interno del servidor" });
       return;
     }
     res.status(200).json(results);
-    console.log(results);
   });
 };
 
+exports.saveBook = (req, res) => {
+  const {
+    titulo,
+    autor,
+    fechaPublicacion,
+    genero,
+    precio,
+    sinopsis,
+    portada
+  } = req.body;
+  
+  const query = `insert into libros (titulo, autor, fechaPublicacion, genero, precio, sinopsis, portada, created_at) values (?, ?, ?, ?, ?, ?, ?, curdate())`;
+
+  const values = [
+    titulo,
+    autor,
+    fechaPublicacion,
+    genero,
+    precio,
+    sinopsis,
+    portada
+  ];
+
+  db.query(query, values, (err, result) => {
+    if (err) {
+      console.error("Error al crear el libro:", err);
+      res.status(500).json({ message: "Error interno del servidor" });
+      return;
+    }
+    res.status(200).json({ message: "Libro guardado exitosamente", id: result.insertId });
+  });
+};
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -2,10 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bookController = require("../controllers/bookController");
 
+// Rutas
 router.get("/", bookController.getAllBooks);
-// router.get("/:id", bookController.getBookById);
-// router.post("/", bookController.createBook);
-// router.put("/:id", bookController.updateBook);
-// router.delete("/:id", bookController.deleteBook);
+router.post("/", bookController.saveBook);
 
 module.exports = router;
