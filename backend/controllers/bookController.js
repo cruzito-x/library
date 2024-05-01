fc3a473174efd1754d3555a2c5dda1d9df4075f8
David Cruz <dcruzer92@gmail.com>
2024-04-30 18:35:36 -0600

    Added: Delete route (really is a update for deleted_at data in existencias table and libros table)

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,7 +1,7 @@
 const db = require("../config/db");
 
 exports.getAllBooks = (req, res) => {
-  db.query("select *, e.existencia as stock from libros l inner join existencias e on e.idLibro = l.idLibro ", (err, results) => {
+  db.query("select *, e.existencia as stock from libros l inner join existencias e on e.idLibro = l.idLibro where (l.deleted_at is null and e.deleted_at is null)", (err, results) => {
     if (err) {
       console.error("Error al obtener los libros:", err);
       res.status(500).json({ message: "Error interno del servidor" });
@@ -12,7 +12,7 @@ exports.getAllBooks = (req, res) => {
 };
 
 exports.getLastFiveBooks = (req, res) => {
-  db.query("select *, e.existencia as stock from libros l inner join existencias e on e.idLibro = l.idLibro order by l.id desc limit 5", (err, results) => {
+  db.query("select *, e.existencia as stock from libros l inner join existencias e on e.idLibro = l.idLibro where (l.deleted_at is null and e.deleted_at is null) order by l.id desc limit 5", (err, results) => {
     if (err) {
       console.error("Error al obtener los libros:", err);
       res.status(500).json({ message: "Error interno del servidor" });
@@ -54,3 +54,22 @@ exports.saveBook = (req, res) => {
     res.status(200).json({ message: "Libro guardado exitosamente", id: result.insertId });
   });
 };
+
+exports.deleteBookUpdatedDeletedAt = (req, res) => {
+  const { idLibro } = req.params;
+
+  const query = `update libros l, existencias e set l.deleted_at = now(), e.deleted_at = now() 
+                 where l.idLibro = ? and e.idLibro = ?`;
+
+  const values = [idLibro, idLibro];
+
+  db.query(query, values, (err, result) => {
+    if (err) {
+      console.error("Error al eliminar el libro:", err);
+      res.status(500).json({ message: "Error interno del servidor" });
+      return;
+    }
+    res.status(200).json({ message: "Libro eliminado exitosamente" });
+  });
+};
+
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const bookController = require("../controllers/bookController");
 
-// Rutas
 router.get("/", bookController.getAllBooks);
 router.get("/latest", bookController.getLastFiveBooks);
 router.post("/", bookController.saveBook);
+router.delete("/deleteBookUpdatedDeletedAt/:idLibro", bookController.deleteBookUpdatedDeletedAt); // Nuevo endpoint
 
 module.exports = router;
