d1a1dcc7421beef8896b81d56017d897a3a18853
David Cruz <dcruzer92@gmail.com>
2024-05-03 11:37:50 -0600

    Added: month sales query and renamed and dashbord queries has renamed

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -5,16 +5,28 @@ exports.getSalesProgress = (req, res) => {
   
 }
 
-exports.getTopSellers = (req, res) => {
-  // Consulta SQL para obtener los libros más vendidos
-  const query = "select l.idLibro, l.titulo, sum(dv.cantidad) as totalVendido from libros l join detalles_venta dv on l.idLibro = dv.idLibro join ventas v on dv.idVenta = v.idVenta where v.estado = 'completado' group by l.idLibro, l.titulo order by totalVendido desc limit 5;";
+exports.getMonthSales = (req, res) => {
+  // Obtener las ventas del mes actual
+  const selectMonthSales = "select v.fecha, sum(v.total) as total_venta from ventas v inner join detalles_venta dv on v.idVenta = dv.idVenta where month(v.fecha) = month(current_date()) and year(v.fecha) = year(current_date()) group by v.fecha";
 
-  db.query(query, (error, results) => {
+  db.query(selectMonthSales, (error, results) => {
+    if (error) {
+      res.status(500).json({ error: 'Error al obtener las ventas del mes actual' });
+    } else {
+      res.json(results);
+    }
+  });
+};
+
+exports.getTopSellers = (req, res) => {
+  // Obtener los libros más vendidos
+  const selectTopSellers = "select l.idLibro, l.titulo, sum(dv.cantidad) as totalVendido from libros l join detalles_venta dv on l.idLibro = dv.idLibro join ventas v on dv.idVenta = v.idVenta where v.estado = 'completado' group by l.idLibro, l.titulo order by totalVendido desc limit 5;";
+
+  db.query(selectTopSellers, (error, results) => {
     if (error) {
       console.error("Error en la consulta SQL:", error);
       res.status(500).json({ error: 'Error al obtener los libros más vendidos' });
     } else {
-      // Formatear los resultados en un JSON con título y totalVendido
       const topSellers = results.map(row => ({
         titulo: row.titulo,
         totalVendido: row.totalVendido
--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const dashboardController = require("../controllers/dashboardController");
 
 router.get("/", dashboardController.getSalesProgress);
+router.get("/monthSales", dashboardController.getMonthSales);
 router.get("/topSellers", dashboardController.getTopSellers);
 
 module.exports = router;
