c2869dea91b5a8aa6d712dc3c3b5e39012f0c3f3
David Cruz <dcruzer92@gmail.com>
2024-05-06 23:40:13 -0600

    Save bills function is updated

--- backend/controllers/billController.js
+++ backend/controllers/billController.js
@@ -2,7 +2,7 @@ const db = require("../config/db");
 const crypto = require("crypto");
 
 exports.getBooks = (req, res) => {
-  db.query("select idLibro as value, titulo as label, autor, precio from libros where deleted_at is null;",
+  db.query("select l.idLibro as value, l.titulo as label, l.autor, l.precio from libros l inner join existencias e on e.idLibro = l.idLibro where l.deleted_at is null and e.existencia > 0;",
   (error, results) => {
     if (error) {
         console.error("Error al obtener los libros:", error);
@@ -17,40 +17,67 @@ exports.getBooks = (req, res) => {
 exports.saveBill = (req, res) => {
   const { selectedBooks, nombre, apellido } = req.body;
 
-  const idVenta = crypto.createHash("md5").update(`${Date.now()}`).digest("hex"); // idVenta
-  const idCliente = crypto.createHash("md5").update(`${Date.now()}`).digest("hex"); // idCliente
-  const fecha = new Date().toISOString().slice(0, 10); // Formato: YYYY-MM-DD esta servirá para created_at de todos los registros
-  const total = selectedBooks.reduce((acumuladorTotal, book) => acumuladorTotal + parseFloat(book.total), 0).toFixed(2);   // Calcular el total de la venta
+  const idVenta = crypto.createHash("md5").update(`${Date.now()}`).digest("hex");
+  const idCliente = crypto.createHash("md5").update(`${Date.now()}`).digest("hex");
+  const total = selectedBooks.reduce((acumuladorTotal, book) => acumuladorTotal + parseFloat(book.total), 0).toFixed(2);
 
-  // Insertar cliente en la tabla clientes
-  const insertCliente = "insert into clientes (idCliente, nombre, apellido, metodoPago, created_at) values (?, ?, ?, ?, curdate())";
-  db.query(insertCliente, [idCliente, nombre, apellido, "efectivo"], (error, result) => {
-    if (error) {
-      console.error("Error al insertar cliente: ", error);
-      res.status(500).json({ message: "Error interno del servidor" });
+  // Begin transaction
+  db.beginTransaction(err => {
+    if (err) {
+      console.error('Error starting transaction: ', err);
+      res.status(500).json({ message: 'Error interno del servidor' });
       return;
     }
 
-    // Insertar venta en la tabla ventas
-      const insertVenta = "insert into ventas (idVenta, fecha, total, estado, created_at) values (?, ?, ?, 'completado', curdate())";
-    db.query(insertVenta, [idVenta, fecha, total], (error, result) => {
+    const insertCliente = "INSERT INTO clientes (idCliente, nombre, apellido, metodoPago, created_at) VALUES (?, ?, ?, ?, CURDATE())";
+    db.query(insertCliente, [idCliente, nombre, apellido, "efectivo"], (error, resultCliente) => {
       if (error) {
-        console.error("Error al insertar venta: ", error);
-        res.status(500).json({ message: "Error interno del servidor" });
-        return;
+        console.error("Error al insertar cliente: ", error);
+        return db.rollback(() => {
+          res.status(500).json({ message: "Error interno del servidor" });
+        });
       }
 
-      // Insertar detalles de venta en la tabla detalles_venta
-      const valuesDetalleVenta = selectedBooks.map(book => [idVenta, idCliente, book.value, book.cantidad, book.precioUnitario, book.subtotal, book.descuento, fecha]);
-      const insertDetalleVenta = "insert into detalles_venta (idVenta, idCliente, idLibro, cantidad, precioUnitario, subtotal, descuento, created_at) values ?";
-      db.query(insertDetalleVenta, [valuesDetalleVenta], (error, result) => {
+      const insertVenta = "INSERT INTO ventas (idVenta, fecha, total, estado, created_at) VALUES (?, CURDATE(), ?, 'completado', CURDATE())";
+      db.query(insertVenta, [idVenta, total], (error, resultVenta) => {
         if (error) {
-          console.error("Error al insertar detalles de venta: ", error);
-          res.status(500).json({ message: "Error interno del servidor" });
-          return;
+          console.error("Error al insertar venta: ", error);
+          return db.rollback(() => {
+            res.status(500).json({ message: "Error interno del servidor" });
+          });
         }
 
-        res.status(200).json({ message: "Factura guardada exitosamente" });
+        const valuesDetalleVenta = selectedBooks.map(book => [idVenta, idCliente, book.value, book.cantidad, book.precioUnitario, book.subtotal, book.descuento, new Date()]);
+        const insertDetalleVenta = "INSERT INTO detalles_venta (idVenta, idCliente, idLibro, cantidad, precioUnitario, subtotal, descuento, created_at) VALUES ?";
+        db.query(insertDetalleVenta, [valuesDetalleVenta], (error, resultDetalleVenta) => {
+          if (error) {
+            console.error("Error al insertar detalles de venta: ", error);
+            return db.rollback(() => {
+              res.status(500).json({ message: "Error interno del servidor" });
+            });
+          }
+
+          const updateExistencia = "UPDATE existencias AS ex INNER JOIN detalles_venta AS dv ON ex.idLibro = dv.idLibro SET ex.existencia = ex.existencia - dv.cantidad WHERE dv.idVenta = ?";
+          db.query(updateExistencia, [idVenta], (error, resultExistencia) => {
+            if (error) {
+              console.error("Error al actualizar existencia: ", error);
+              return db.rollback(() => {
+                res.status(500).json({ message: "Error interno del servidor" });
+              });
+            }
+
+            // Commit the transaction if all queries were successful
+            db.commit(err => {
+              if (err) {
+                console.error("Error committing transaction: ", err);
+                return db.rollback(() => {
+                  res.status(500).json({ message: "Error interno del servidor" });
+                });
+              }
+              res.status(200).json({ message: "Factura guardada exitosamente" });
+            });
+          });
+        });
       });
     });
   });
