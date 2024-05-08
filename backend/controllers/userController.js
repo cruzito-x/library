cf4def186fcce6d6657544934eacc37d356a7d5c
David Cruz <dcruzer92@gmail.com>
2024-05-08 00:08:01 -0600

    Users crud is 75% finished

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,3 +47,20 @@ exports.deleteUserUpdatedAt = (req, res) => {
     res.status(200).json({ message: "Usuario eliminado exitosamente" });
   });
 }
+
+exports.updateUser = (req, res) => {
+  const { idUsuario } = req.params;
+  const { nombreUsuario, password, rol } = req.body;
+
+  const updateUserQuery = `update usuarios set nombreUsuario = ?, password = ?, rol = ? where idUsuario = ?`;
+  const values = [nombreUsuario, password, rol, idUsuario];
+
+  db.query(updateUserQuery, values, (error, result) => {
+    if (error) {
+      console.error("Error al actualizar el usuario:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+      return;
+    }
+    res.status(200).json({ message: "Usuario actualizado exitosamente" });
+  });
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/userController");
 router.get("/", userController.getUsers);
 router.post("/save", userController.saveUser);
 router.delete("/deleteUserUpdatedDeletedAt/:idUsuario", userController.deleteUserUpdatedAt);
+router.put("/updateUser/:idUsuario", userController.updateUser);
 
 module.exports = router;
