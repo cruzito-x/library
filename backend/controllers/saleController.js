b8d640de7f050498b592194990de8105c0354722
David Cruz <dcruzer92@gmail.com>
2024-05-11 12:34:09 -0600

    Report queries has been finished

--- backend/controllers/saleController.js
+++ backend/controllers/saleController.js
@@ -94,8 +94,62 @@ exports.getSalesData = (req, res) => {
         from ventas where fecha >= date_sub(curdate(), interval 1 month) and fecha <= curdate();`);
   }
   if (period == 180) {
+    selectSalesByPeriod.push(
+      "select l.titulo, count(dv.idLibro) as ventas from detalles_venta dv join libros l on dv.idLibro = l.idLibro join ventas v on dv.idVenta = v.idVenta where v.fecha >= date_sub(curdate(), interval 6 month) group by dv.idLibro order by ventas desc limit 5;"
+    ); // Obtener los 5 libros más vendidos en los últimos 6 meses.
+
+    selectSalesByPeriod.push(
+      'select sum(dv.cantidad) as total_libros_vendidos, concat("desde ", date_sub(curdate(), interval 6 month), " hasta ", curdate()) as periodo from detalles_venta dv join ventas v on dv.idVenta = v.idVenta where v.fecha >= date_sub(curdate(), interval 6 month);'
+    ); // Obtener el periodo de reporte y el total de libros vendidos.
+
+    selectSalesByPeriod.push(
+      "select g.nombreGenero, count(dv.idLibro) as ventas from detalles_venta dv join libros l on dv.idLibro = l.idLibro join genero g on l.genero = g.idGenero join ventas v on dv.idVenta = v.idVenta where v.fecha >= date_sub(curdate(), interval 6 month) group by l.genero order by ventas desc limit 5;"
+    ); // Obtener los géneros más populares en los últimos 6 meses.
+
+    selectSalesByPeriod.push(
+      "select sum(dv.subtotal) as total_ganancias from detalles_venta dv join ventas v on dv.idVenta = v.idVenta where v.fecha >= date_sub(curdate(), interval 6 month);"
+    ); // Obtener el total de ganancias en los últimos 6 meses.
+
+    selectSalesByPeriod.push(
+      "select titulo, precio from libros where created_at >= date_sub(now(), interval 6 month) and deleted_at is null order by created_at desc limit 10;"
+    ); // Obtener la lista de los 10 registros más recientes de libros de los últimos 6 meses
+
+    selectSalesByPeriod.push(`select sum(case when fecha >= date_sub(curdate(), interval 6 month) and fecha <= curdate() then total else 0 end) as ventas_ultimos_6_meses, 
+    sum(case when fecha = curdate() then total else 0 end) as ventas_hoy, concat(
+      case when sum(case when fecha = curdate() then total else 0 end) > sum(case when fecha >= date_sub(curdate(), interval 6 month) and fecha <= date_sub(curdate(), interval 5 month) then total else 0 end) then "incremento del " else "decremento del " end,
+      case when sum (case when fecha >= date_sub(curdate(), interval 6 month) and fecha <= date_sub(curdate(), interval 5 month) then total else 0 end) = 0 then "[incremento infinito]"
+      else round(abs(((sum(case when fecha = curdate() then total else 0 end) - sum(case when fecha >= date_sub(curdate(), interval 6 month) and fecha <= date_sub(curdate(), interval 5 month) then total else 0 end)) / sum(case
+        when fecha >= date_sub(curdate(), interval 6 month) and fecha <= date_sub(curdate(), interval 5 month) then total else 0 end)) * 100)) end,"%") as porcentaje_incremento_decremento
+        from ventas where fecha >= date_sub(curdate(), interval 6 month) and fecha <= curdate();`);
   }
   if (period == 365) {
+    selectSalesByPeriod.push(
+      "select l.titulo, count(dv.idLibro) as ventas from detalles_venta dv join libros l on dv.idLibro = l.idLibro join ventas v on dv.idVenta = v.idVenta where v.fecha >= date_sub(curdate(), interval 1 year) group by dv.idLibro order by ventas desc limit 5;"
+    ); // Obtener los 5 libros más vendidos en el último año.
+
+    selectSalesByPeriod.push(
+      'select sum(dv.cantidad) as total_libros_vendidos, concat("desde ", date_sub(curdate(), interval 1 year), " hasta ", curdate()) as periodo from detalles_venta dv join ventas v on dv.idVenta = v.idVenta where v.fecha >= date_sub(curdate(), interval 1 year);'
+    ); // Obtener el periodo de reporte y el total de libros vendidos.
+
+    selectSalesByPeriod.push(
+      "select g.nombreGenero, count(dv.idLibro) as ventas from detalles_venta dv join libros l on dv.idLibro = l.idLibro join genero g on l.genero = g.idGenero join ventas v on dv.idVenta = v.idVenta where v.fecha >= date_sub(curdate(), interval 1 year) group by l.genero order by ventas desc limit 5;"
+    ); // Obtener los géneros más populares en el último año.
+
+    selectSalesByPeriod.push(
+      "select sum(dv.subtotal) as total_ganancias from detalles_venta dv join ventas v on dv.idVenta = v.idVenta where v.fecha >= date_sub(curdate(), interval 1 year);"
+    ); // Obtener el total de ganancias en el último año.
+
+    selectSalesByPeriod.push(
+      "select titulo, precio from libros where created_at >= date_sub(now(), interval 1 year) and deleted_at is null order by created_at desc limit 10;"
+    ); // Obtener la lista de los 10 registros más recientes de libros del último año
+
+    selectSalesByPeriod.push(`select sum(case when fecha >= date_sub(curdate(), interval 1 year) and fecha <= curdate() then total else 0 end) as ventas_ultimo_anio, 
+    sum(case when fecha = curdate() then total else 0 end) as ventas_hoy, concat(
+      case when sum(case when fecha = curdate() then total else 0 end) > sum(case when fecha >= date_sub(curdate(), interval 1 year) and fecha <= date_sub(curdate(), interval 11 month) then total else 0 end) then "incremento del " else "decremento del " end,
+      case when sum (case when fecha >= date_sub(curdate(), interval 1 year) and fecha <= date_sub(curdate(), interval 11 month) then total else 0 end) = 0 then "[incremento infinito]"
+      else round(abs(((sum(case when fecha = curdate() then total else 0 end) - sum(case when fecha >= date_sub(curdate(), interval 1 year) and fecha <= date_sub(curdate(), interval 11 month) then total else 0 end)) / sum(case
+        when fecha >= date_sub(curdate(), interval 1 year) and fecha <= date_sub(curdate(), interval 11 month) then total else 0 end)) * 100)) end,"%") as porcentaje_incremento_decremento
+        from ventas where fecha >= date_sub(curdate(), interval 1 year) and fecha <= curdate();`);
   }
 
   const promises = selectSalesByPeriod.map((query) => {
