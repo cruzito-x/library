dd220f7bbe55247791345940eecf04c75a02367a
David Cruz <dcruzer92@gmail.com>
2024-05-24 21:25:14 -0600

    Frontend has been finished! All UI has been updated

---
+++ frontend/src/components/billInvoicePDF/BillInvoicePDF.jsx
@@ -0,0 +1,158 @@
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import { PrinterOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+
+const BillInvoicePDF = ({
+  data,
+  selectedBooks,
+  totalSubTotal,
+  totalDiscount,
+}) => {
+  const generatePDF = () => {
+    const doc = new jsPDF();
+
+    // Añadir logo y título
+    const logo = new Image();
+    logo.src = "logo.png";
+    doc.addImage(
+      logo,
+      "PNG",
+      doc.internal.pageSize.getWidth() - 52,
+      12,
+      50,
+      27
+    );
+    doc.setTextColor("#001529");
+    doc.setFontSize(32);
+    doc.text("FACTURA", 15, 30);
+    doc.setFont("Helvetica", "bold");
+
+    // Añadir detalles de cliente
+    doc.setFontSize(12);
+    doc.text("Facturado por:", 15, 50);
+    doc.setFont("Helvetica");
+    doc.text(localStorage.getItem("username"), 15, 55);
+    doc.text(`Cliente: ${data.nombre} ${data.apellido}`, 15, 65);
+    const currentDate = new Date();
+    const formattedDate = `${currentDate.getFullYear()}-${(
+      currentDate.getMonth() + 1
+    )
+      .toString()
+      .padStart(2, "0")}-${currentDate.getDate().toString().padStart(2, "0")}`;
+    doc.text("Fecha:", doc.internal.pageSize.getWidth() - 23, 50, {
+      align: "right",
+    });
+    doc.setFont("Helvetica");
+    doc.text(formattedDate, doc.internal.pageSize.getWidth() - 15, 55, {
+      align: "right",
+    });
+
+    // Añadir línea arriba de la tabla
+    doc.setLineWidth(0.2);
+    doc.setDrawColor("#001529");
+    doc.line(15, 80, doc.internal.pageSize.getWidth() - 15, 80);
+
+    // Añadir tabla
+    const tableData = [];
+    const totalPagar = selectedBooks.reduce(
+      (acumuladorTotal, book) => acumuladorTotal + parseFloat(book.total),
+      0
+    );
+
+    selectedBooks.forEach((book) => {
+      const rowData = [
+        { content: book.label, styles: { halign: "left" } },
+        { content: `$${book.precio}`, styles: { halign: "center" } },
+        { content: book.cantidad, styles: { halign: "center" } },
+        { content: `$${book.total}`, styles: { halign: "center" } },
+      ];
+      tableData.push(rowData);
+    });
+
+    doc.autoTable({
+      head: [
+        [
+          { content: "Nombre libro", styles: { halign: "left" } },
+          { content: "Precio", styles: { halign: "center" } },
+          { content: "Cantidad", styles: { halign: "center" } },
+          { content: "Total", styles: { halign: "center" } },
+        ],
+      ],
+      body: tableData,
+      startY: 85,
+      theme: "plain",
+      styles: {
+        textColor: "#001529",
+      },
+    });
+
+    // Añadir línea abajo de la tabla
+    doc.setLineWidth(0.2);
+    doc.setDrawColor("#001529");
+    doc.line(
+      15,
+      doc.autoTable.previous.finalY + 5,
+      doc.internal.pageSize.getWidth() - 15,
+      doc.autoTable.previous.finalY + 5
+    );
+
+    // Add payment method and totals
+    doc.text("Método de pago", 15, doc.autoTable.previous.finalY + 15);
+    doc.setFont("Helvetica");
+    doc.text("Efectivo", 15, doc.autoTable.previous.finalY + 20);
+
+    doc.text(
+      "Subtotal:",
+      doc.internal.pageSize.getWidth() - 75,
+      doc.autoTable.previous.finalY + 15
+    );
+    doc.text(
+      `$${totalSubTotal}`,
+      doc.internal.pageSize.getWidth() - 15,
+      doc.autoTable.previous.finalY + 15,
+      { align: "right" }
+    );
+
+    doc.text(
+      "Descuento:",
+      doc.internal.pageSize.getWidth() - 75,
+      doc.autoTable.previous.finalY + 20
+    );
+    doc.text(
+      `$${totalDiscount}`,
+      doc.internal.pageSize.getWidth() - 15,
+      doc.autoTable.previous.finalY + 20,
+      { align: "right" }
+    );
+
+    doc.text(
+      "Total:",
+      doc.internal.pageSize.getWidth() - 75,
+      doc.autoTable.previous.finalY + 25
+    );
+    doc.text(
+      `$${totalPagar.toFixed(2)}`,
+      doc.internal.pageSize.getWidth() - 15,
+      doc.autoTable.previous.finalY + 25,
+      { align: "right" }
+    );
+
+    const now = new Date();
+    const formattedNow = `${now.getFullYear()}${pad(now.getMonth() + 1)}${pad(
+      now.getDate()
+    )}${pad(now.getHours())}${pad(now.getMinutes())}${pad(now.getSeconds())}`;
+
+    doc.save(formattedNow + ".pdf");
+  };
+
+  const pad = (number) => (number < 10 ? `0${number}` : number.toString());
+
+  return (
+    <Button type="primary" onClick={generatePDF}>
+      <PrinterOutlined /> Generar Factura
+    </Button>
+  );
+};
+
+export default BillInvoicePDF;
--- frontend/src/views/bills/Bills.jsx
+++ frontend/src/views/bills/Bills.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { UserOutlined, PlusOutlined, MinusOutlined, DeleteOutlined, PrinterOutlined } from "@ant-design/icons";
+import { UserOutlined, PlusOutlined, MinusOutlined, DeleteOutlined } from "@ant-design/icons";
 import {
   Breadcrumb,
   Form,
@@ -18,8 +18,7 @@ import {
   theme,
   message
 } from "antd";
-import jsPDF from "jspdf";
-import "jspdf-autotable";
+import BillInvoicePDF from "../../components/billInvoicePDF/BillInvoicePDF";
 
 const Bills = () => {
   const { Content } = Layout;
@@ -51,7 +50,7 @@ const Bills = () => {
         console.error(error.message);
       });
   }, [form]);
-  
+
   const handleChange = (value) => {
     const selectedBook = books.find((book) => book.value === value);
     form.setFieldsValue({ precioUnitario: selectedBook.precio });
@@ -84,18 +83,12 @@ const Bills = () => {
       .then((response) => response.json())
       .then((data) => {
         message.success(data.message);
-        generatePDF(request, selectedBooks);
         setSelectedBooks([]);
         form.setFieldsValue({ nombre: "", apellido: "" });
       })
       .catch((error) => {
         message.error(error.message);
       });
-  };
-
-  const handleBookSelect = (value, option) => {
-    const selectedBook = books.find((book) => book.value === value);
-    form.setFieldsValue({ precioUnitario: selectedBook.precio });
   };
 
   const handleAddBook = () => {
@@ -154,34 +147,33 @@ const Bills = () => {
 
   const handleIncreaseBook = (value) => {
     const updatedBooks = selectedBooks.map((book) => {
-        if (book.value === value) {
-            const newCantidad = book.cantidad + 1;
-            const selectedBook = books.find((b) => b.value === value);
-            if (newCantidad > selectedBook.existencia) {
-                message.warning(
-                    `Sólo hay ${selectedBook.existencia} libros disponibles.`
-                );
-                return book;
-            }
-            if (newCantidad > 10) { // Limitar a un máximo de 10 unidades
-                message.warning("No se pueden añadir más de 10 unidades.");
-                return book;
-            }
-            const newSubtotal = (newCantidad * book.precioUnitario).toFixed(2);
-            const newDescuento = book.descuento;
-            const newTotal = (newSubtotal - newDescuento).toFixed(2);
-            return {
-                ...book,
-                cantidad: newCantidad,
-                subtotal: newSubtotal,
-                total: newTotal,
-            };
+      if (book.value === value) {
+        const newCantidad = book.cantidad + 1;
+        const selectedBook = books.find((b) => b.value === value);
+        if (newCantidad > selectedBook.existencia) {
+          message.warning(
+            `Sólo hay ${selectedBook.existencia} libros disponibles.`
+          );
+          return book;
         }
-        return book;
+        if (newCantidad > 10) {
+          message.warning("No se pueden añadir más de 10 unidades.");
+          return book;
+        }
+        const newSubtotal = (newCantidad * book.precioUnitario).toFixed(2);
+        const newDescuento = book.descuento;
+        const newTotal = (newSubtotal - newDescuento).toFixed(2);
+        return {
+          ...book,
+          cantidad: newCantidad,
+          subtotal: newSubtotal,
+          total: newTotal,
+        };
+      }
+      return book;
     });
     setSelectedBooks(updatedBooks);
   };
-
 
   const handleReduceBook = (value) => {
     const updatedBooks = selectedBooks
@@ -243,156 +235,6 @@ const Bills = () => {
   const totalSubTotal = getTotalSubTotal();
   const totalDiscount = getTotalDiscount();
   const totalAmount = getTotalAmount();
-
-  const generatePDF = (data, selectedBooks) => {
-    const doc = new jsPDF();
-
-    // Agregar el logo y el título "Factura"
-    const logo = new Image();
-    logo.src = "logo.png";
-    doc.addImage(
-      logo,
-      "PNG",
-      doc.internal.pageSize.getWidth() - 52,
-      12,
-      50,
-      27
-    );
-    doc.setTextColor("#001529");
-    doc.setFontSize(32);
-    doc.text("FACTURA", 15, 30);
-    doc.setFont("Helvetica", "bold");
-
-    // Agregar detalles del cliente y factura
-    doc.setFontSize(12);
-    doc.text("Facturado por:", 15, 50);
-    doc.setFont("Helvetica");
-    doc.text(localStorage.getItem("username"), 15, 55);
-    doc.text(`Cliente: ${data.nombre} ${data.apellido}`, 15, 65);
-    const currentDate = new Date();
-    const formattedDate = `${currentDate.getFullYear()}-${(
-      currentDate.getMonth() + 1
-    )
-      .toString()
-      .padStart(2, "0")}-${currentDate.getDate().toString().padStart(2, "0")}`;
-    doc.text("Fecha:", doc.internal.pageSize.getWidth() - 23, 50, {
-      align: "right",
-    });
-    doc.setFont("Helvetica");
-    doc.text(formattedDate, doc.internal.pageSize.getWidth() - 15, 55, {
-      align: "right",
-    });
-
-    // Agregar una línea arriba de la tabla (10 unidades más arriba)
-    doc.setLineWidth(0.2);
-    doc.setDrawColor("#001529");
-    doc.line(15, 80, doc.internal.pageSize.getWidth() - 15, 80);
-
-    // Agregar la tabla centrada
-    const tableData = [];
-    const totalPagar = selectedBooks.reduce(
-      (acumuladorTotal, book) => acumuladorTotal + parseFloat(book.total),
-      0
-    );
-    const totalPagarNumber = parseFloat(totalPagar);
-
-    selectedBooks.forEach((book, index) => {
-      const rowData = [
-        { content: book.label, styles: { halign: "left" } },
-        { content: `$${book.precio}`, styles: { halign: "center" } },
-        { content: book.cantidad, styles: { halign: "center" } },
-        { content: `$${book.total}`, styles: { halign: "center" } },
-      ];
-      tableData.push(rowData);
-    });
-    doc.autoTable({
-      head: [
-        [
-          { content: "Nombre libro", styles: { halign: "left" } },
-          { content: "Precio", styles: { halign: "center" } },
-          { content: "Cantidad", styles: { halign: "center" } },
-          { content: "Total", styles: { halign: "center" } },
-        ],
-      ],
-      body: tableData,
-      startY: 85,
-      theme: "plain",
-      styles: {
-        textColor: "#001529",
-      },
-    });
-
-    // Agregar una línea abajo de la tabla
-    doc.setLineWidth(0.2);
-    doc.setDrawColor("#001529");
-    doc.line(
-      15,
-      doc.autoTable.previous.finalY + 5,
-      doc.internal.pageSize.getWidth() - 15,
-      doc.autoTable.previous.finalY + 5
-    );
-
-    // Agregar Método de Pago (sin negrita/bold)
-    doc.text("Método de pago", 15, doc.autoTable.previous.finalY + 15);
-    doc.setFont("Helvetica");
-    doc.text("Efectivo", 15, doc.autoTable.previous.finalY + 20);
-
-    // Agregar Subtotal a la misma altura que Método de pago (sin negrita/bold)
-    doc.text(
-      "Subtotal:",
-      doc.internal.pageSize.getWidth() - 75,
-      doc.autoTable.previous.finalY + 15
-    );
-    doc.text(
-      `$${totalSubTotal}`,
-      doc.internal.pageSize.getWidth() - 15,
-      doc.autoTable.previous.finalY + 15,
-      { align: "right" }
-    );
-
-    // Agregar Descuento y Total
-    doc.text(
-      "Descuento:",
-      doc.internal.pageSize.getWidth() - 75,
-      doc.autoTable.previous.finalY + 20
-    );
-    doc.text(
-      `$${totalDiscount}`,
-      doc.internal.pageSize.getWidth() - 15,
-      doc.autoTable.previous.finalY + 20,
-      { align: "right" }
-    );
-    doc.text(
-      "Total:",
-      doc.internal.pageSize.getWidth() - 75,
-      doc.autoTable.previous.finalY + 25
-    );
-    doc.text(
-      `$${totalPagarNumber.toFixed(2)}`,
-      doc.internal.pageSize.getWidth() - 15,
-      doc.autoTable.previous.finalY + 25,
-      { align: "right" }
-    );
-
-    let now = new Date();
-    let formattedNow =
-      now.getFullYear().toString() +
-      pad(now.getMonth() + 1) +
-      pad(now.getDate()) +
-      pad(now.getHours()) +
-      pad(now.getMinutes()) +
-      pad(now.getSeconds());
-
-    // Guardar el PDF
-    doc.save(formattedNow + ".pdf");
-  };
-
-  function pad(number) {
-    if (number < 10) {
-      return "0" + number;
-    }
-    return number;
-  }
 
   const columns = [
     {
@@ -471,7 +313,9 @@ const Bills = () => {
   return (
     <Content style={{ margin: "0 16px" }}>
       <Breadcrumb style={{ margin: "50px 0 16px 0" }}>
-        <Breadcrumb.Item><UserOutlined/> {localStorage.getItem("username")}</Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <UserOutlined /> {localStorage.getItem("username")}
+        </Breadcrumb.Item>
         <Breadcrumb.Item>Facturar</Breadcrumb.Item>
       </Breadcrumb>
       <div
@@ -486,17 +330,35 @@ const Bills = () => {
         <Form layout="vertical" form={form}>
           <Row gutter={[16, 16]}>
             <Col xs={24} sm={12} md={8}>
-              <Form.Item label="Libros" name="libro" rules={[{ required: true, message: "Por favor seleccione un libro" }]}>
-                <Select options={books} onChange={handleChange}/>
+              <Form.Item
+                label="Libros"
+                name="libro"
+                rules={[{ required: true, message: "Por favor seleccione un libro" }]}
+              >
+                <Select options={books} onChange={handleChange} />
               </Form.Item>
             </Col>
             <Col xs={24} sm={12} md={8}>
-              <Form.Item label="Cantidad" name="cantidad" rules={[{ required: true, message: "Por favor introduzca la cantidad" }]}>
-                <InputNumber min={1} max={10} placeholder="Introduzca una cantidad entre 1 y 10" defaultValue={1} style={{ width: "100%" }} />
+              <Form.Item
+                label="Cantidad"
+                name="cantidad"
+                rules={[{ required: true, message: "Por favor introduzca la cantidad" }]}
+              >
+                <InputNumber
+                  min={1}
+                  max={10}
+                  placeholder="Introduzca una cantidad entre 1 y 10"
+                  defaultValue={1}
+                  style={{ width: "100%" }}
+                />
               </Form.Item>
             </Col>
             <Col xs={24} sm={12} md={8}>
-              <Form.Item label="Nombre" name="nombre" rules={[{ required: true, message: "Por favor introduzca el nombre" }]}>
+              <Form.Item
+                label="Nombre"
+                name="nombre"
+                rules={[{ required: true, message: "Por favor introduzca el nombre" }]}
+              >
                 <Input prefix={<UserOutlined />} placeholder="Nombre" />
               </Form.Item>
             </Col>
@@ -504,16 +366,34 @@ const Bills = () => {
           <Row gutter={16}>
             <Col xs={24} sm={12} md={8}>
               <Form.Item label="Precio Unitario" name="precioUnitario">
-                <Input prefix="$" disabled style={{ backgroundColor: "#f5f5f5", color: "black" }} />
+                <Input
+                  prefix="$"
+                  disabled
+                  style={{ backgroundColor: "#f5f5f5", color: "black" }}
+                />
               </Form.Item>
             </Col>
             <Col xs={24} sm={12} md={8}>
-              <Form.Item label="Descuento (%)" name="descuento" initialValue={0} rules={[{ type: "number", min: 0, max: 90, message: "Introduzca un descuento válido (0-90)" }]}>
-                <InputNumber min={0} max={90} defaultValue={0} style={{ width: "100%" }} />
+              <Form.Item
+                label="Descuento (%)"
+                name="descuento"
+                initialValue={0}
+                rules={[{ type: "number", min: 0, max: 90, message: "Introduzca un descuento válido (0-90)" }]}
+              >
+                <InputNumber
+                  min={0}
+                  max={90}
+                  defaultValue={0}
+                  style={{ width: "100%" }}
+                />
               </Form.Item>
             </Col>
             <Col xs={24} sm={12} md={8}>
-              <Form.Item label="Apellido" name="apellido" rules={[{ required: true, message: "Por favor introduzca el apellido" }]}>
+              <Form.Item
+                label="Apellido"
+                name="apellido"
+                rules={[{ required: true, message: "Por favor introduzca el apellido" }]}
+              >
                 <Input prefix={<UserOutlined />} placeholder="Apellido" />
               </Form.Item>
             </Col>
@@ -521,7 +401,11 @@ const Bills = () => {
           <Row gutter={16}>
             <Col xs={24} sm={12} md={8}>
               <Form.Item>
-                <Button type="primary" onClick={handleAddBook} style={{ marginTop: "29px", width: "100%" }}>
+                <Button
+                  type="primary"
+                  onClick={handleAddBook}
+                  style={{ marginTop: "29px", width: "100%" }}
+                >
                   Agregar Libro
                 </Button>
               </Form.Item>
@@ -557,9 +441,12 @@ const Bills = () => {
           }}
         />
         <Divider />
-        <Button type="primary" onClick={handleSaveBill}>
-          <PrinterOutlined/> Generar Factura
-        </Button>
+        <BillInvoicePDF
+          data={form.getFieldsValue(["nombre", "apellido"])}
+          selectedBooks={selectedBooks}
+          totalSubTotal={totalSubTotal}
+          totalDiscount={totalDiscount}
+        />
       </div>
     </Content>
   );
