c0aad56a49c19baa1a553b27d8c3ac85ec084b44
David Cruz <dcruzer92@gmail.com>
2024-06-27 21:28:51 -0600

    IP has bbeen updated, added for local test the following deps: Jest and Supertest

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const helmet = require("helmet");
 
 const app = express();
 const port = 3001;
-const ip = "192.168.0.6";
+const ip = "192.168.0.5";
 
 const logsFolder = path.join(__dirname, "./logs"); // Ruta de la carpeta de logs
 
